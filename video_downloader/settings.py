c47ef1225060d5f4cf1a90c87c4391f14a9b370e
Deepudholiwal <dk4796804@gmail.com>
2024-11-15 17:20:27 +0530

    Update ALLOWED_HOSTS with Vercel domain

--- video_downloader/settings.py
+++ video_downloader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c9!!*i%ejrlbig!rnd7&(ojo4zlk5)1p1vwsdcrv*pg=77o+ga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['video-downloader-bgfd3pely-dk4796804-gmailcoms-projects.vercel.app']
 
 
 
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'video_downloader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Ensure the correct database engine
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # Password validation
@@ -118,11 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
